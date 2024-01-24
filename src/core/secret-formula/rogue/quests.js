defb6a3459ae74ff465bc0266d05536c32929b97
Spotky1004 <spotky1004@gmail.com>
2024-01-25 00:31:53 +0900

    Add 3 quests

--- src/core/secret-formula/rogue/quests.js
+++ src/core/secret-formula/rogue/quests.js
@@ -77,6 +77,32 @@ addQuest({
   isUnlocked: () => true,
   rewardTable: [[4, 1001], [3, 1002], [2, 1003], [9, 1004], [5, 1005]],
 });
+addQuest({
+  id: 103,
+  type: "normal",
+  name: () => "(90° × ∞)²",
+  description: () => `Have 8 8th Antimatter Dimension`,
+  getProgress: () => clampProgress(window.player.dimensions.antimatter[7].amount.div(8)),
+  isUnlocked: () => window.player.rogue.questCompleted[101],
+  rewardTable: [[1, 1002], [1, 1004]]
+});
+addQuest({
+  id: 104,
+  type: "normal",
+  name: () => "666",
+  description: () => `Have exactly 6 Dim Boosts, 6 6th Antimatter Dimension`,
+  getProgress: () => {
+    const dimBoost = window.player.dimensionBoosts;
+    const dim6 = window.player.dimensions.antimatter[5].amount.toNumber();
+
+    let progress = 0;
+    if (dimBoost <= 6) progress += dimBoost / 12;
+    if (dim6 <= 6) progress += dim6 / 12;
+    return clampProgress(progress);
+  },
+  isUnlocked: () => window.player.dimensionBoosts > 0,
+  rewardTable: [[1, 1001], [1, 1002], [1, 1003], [1, 1004], [1, 1005]]
+});
 
 // Debuff
 addQuest({
@@ -87,6 +113,15 @@ addQuest({
   getProgress: () => clampProgress(window.player.dimensionBoosts),
   isUnlocked: () => false,
   rewardTable: [[1, 2001], [1, 2002]],
+});
+addQuest({
+  id: 202,
+  type: "debuff",
+  name: () => 'More <i class="fas fa-fire"></i>',
+  description: () => `Lose 10 <i class="fas fa-heart"></i>`,
+  getProgress: () => clampProgress(Currency.maxHp.value.sub(Currency.hp.value).div(10)),
+  isUnlocked: () => false,
+  rewardTable: [[5, 2001], [1, 2002]]
 });
 
 const normalQuests = [...quests.entries()]
